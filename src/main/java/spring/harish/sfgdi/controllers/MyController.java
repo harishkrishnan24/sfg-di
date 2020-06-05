86bd00e6a8968846e8b72a402f3c954118d298ab
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-06-05 16:56:36 +0530

    Add primary service

--- src/main/java/spring/harish/sfgdi/controllers/MyController.java
+++ src/main/java/spring/harish/sfgdi/controllers/MyController.java
@@ -1,12 +1,18 @@
 package spring.harish.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
+import spring.harish.sfgdi.services.GreetingService;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello() {
-        System.out.println("Hello World");
-        return "Hello World";
+        return greetingService.sayGreeting();
     }
 }
